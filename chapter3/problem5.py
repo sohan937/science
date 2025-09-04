f0fb6cbcc96804762517b302721de8d5ece9a7e3
sohan kumar <sohansk9135@gmail.com>
2025-09-04 15:33:54 +0530

    problem5

---
+++ chapter3/problem5.py
@@ -0,0 +1,6 @@
+# write a program to format the following letters using escape sequence characters
+# Original letter (single line)
+letter = "Dear Sagar, this Python course is nice. Thanks!"
+
+formatted_letter = "Dear Sagar,\n\tThis Python course is nice.\nThanks!"
+print(formatted_letter)
