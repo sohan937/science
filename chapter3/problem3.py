7b142a3efc547d2d2ddc79d54bf4df0ec8e7988d
sohan kumar <sohansk9135@gmail.com>
2025-09-04 15:26:02 +0530

    doublespaces

---
+++ chapter3/problem3.py
@@ -0,0 +1,5 @@
+# write a program to double spaces in a string
+name="sagar is good boy"
+print(name.find("  "))     #output=-1   mtlb double space nhi hai isme
+name1="sagar is a good  boy"
+print(name1.find("  "))      # double space at index 15
