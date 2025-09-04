65a00a22f8846b9ee6f4299557e865a877447fd5
sohan kumar <sohansk9135@gmail.com>
2025-09-04 10:48:00 +0530

    var

--- chapter2/variable.py
+++ chapter2/variable.py
@@ -1,1 +1,12 @@
 # variable:variable is a name given to memorylocaton in program
+# keywords:reserve word in a python
+# identifier=class/function/variable name
+# Datatypes:that specifies which type of value a variable can hold and,consequently,what operations can be performed on that data
+
+
+
+
+a=1
+b=2
+print(a+b)   #iska output 3
+print("a+b")    #iska output a+b
