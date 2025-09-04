c6a4e53ba45845baec5059ca6eecc2e91b05c303
sohan kumar <sohansk9135@gmail.com>
2025-09-04 15:19:23 +0530

    template

---
+++ chapter3/problem2.py
@@ -0,0 +1,11 @@
+# write a program to fill in a letter template given below with name and date
+letter = """
+Dear <|Name|>,
+You are selected!
+<|Date|>
+"""
+name = input("Enter your name: ")
+date = input("Enter date: ")
+letter = letter.replace("<|Name|>", name)
+letter = letter.replace("<|Date|>", date)
+print(letter)
