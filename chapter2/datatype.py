61b3c4e7fd9bb5a44853af1e81717d785a84d341
sohan kumar <sohansk9135@gmail.com>
2025-09-04 11:20:09 +0530

    datatype

---
+++ chapter2/datatype.py
@@ -0,0 +1,36 @@
+# Datatypes:that specifies which type of value a variable can hold and,consequently,what operations can be performed on that data.integer,float,string,boolean,none
+# python is a fantastic language that automatically identifies the type of data for us.
+a=1   # is a integer
+b=5.2   #floating number
+c="sagar"   #strings
+d=False    # d is boolean variable
+e=None     # e is none type variable:  jab kisi varible me kuch v nhi ho to non type variable ka use krte hai
+
+                 # Rules for defining a variable name
+'''
+ 1.   A variable name contain alphbets,digits,and underscore 
+ 2.   A variable name can only start with an alphbets and undercores
+ 3.   A variable name can't start with digit
+ 4.   No while space is allowed to be used inside a variable name   
+ '''    
+
+
+  
+          # example  
+'''          
+a=2     ye valid h
+aaa=5    valid h
+# 6544221=num   ye error dega
+sagar=45
+_num=5
+@sagar=56    #invalid h
+s@agar=56   #invalid hai
+arm number=729   ye invalid hai q ki bich me space h  
+'''
+
+
+
+
+
+
+
